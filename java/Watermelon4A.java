47672f91a4887b825ea0e5c8c99536bf16d46c1b
smtydn <sametaydin1997@gmail.com>
2021-06-01 01:11:30 +0300

    renaming

--- java/Watermelon4A.java
+++ java/Watermelon4A.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class Watermelon_4A {
+public class Watermelon4A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
